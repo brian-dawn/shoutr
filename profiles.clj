8399693424e37b9e365e7d1b6b66969c51b29b21
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-08-18 21:50:32 -0500

    Update .gitignore, add correct DB username and pw

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,2 @@
-{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/shoutr_dev?user=db_user_name_here&password=db_user_password_here"}}
- :profiles/test {:env {:database-url "jdbc:postgresql://localhost/shoutr_test?user=db_user_name_here&password=db_user_password_here"}}}
+{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/shoutr_dev?user=shoutr&password=shoutr"}}
+ :profiles/test {:env {:database-url "jdbc:postgresql://localhost/shoutr_test?user=shoutr&password=shoutr"}}}
