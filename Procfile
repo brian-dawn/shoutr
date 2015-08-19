a00a20517ec8097b8b63a265bb6e38f131e6a8d9
Brian Dawn <brian.t.dawn@gmail.com>
2015-08-18 20:48:59 -0500

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/shoutr.jar clojure.main -m shoutr.core
---
+++ env/prod/cljs/shoutr/prod.cljs
@@ -0,0 +1,7 @@
+(ns shoutr.app
+  (:require [shoutr.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
